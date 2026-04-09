929e08fd3da7c5af4f9cee02291265f7f3290e1b
kxxdree <truebackward@gmail.com>
2026-04-09 22:15:00 +0300

    feat: add body overflow style

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ const applicationModal = document.querySelector(".application-modal");
 const applicationButton = document.querySelector("#apply-button");
 applicationButton.addEventListener("click", () => {
   applicationModal.style.display = "flex";
+  document.body.style.overflow = "hidden";
 });
 
 const closeApplicationButton = document.querySelector(".application-modal-close-button");
 closeApplicationButton.addEventListener("click", () => {
   applicationModal.style.display = "none";
+  document.body.style.overflow = "scroll";
 });
