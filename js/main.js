424b7bc574b27565be2e92ed7caeb8b5241e9172
kxxdree <truebackward@gmail.com>
2026-04-10 16:44:52 +0300

    feat: wip search

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 const applicationModal = document.querySelector(".application-modal");
 
+const addBodyStyles = () => {
+  document.body.style.overflow = "hidden";
+  document.body.style.paddingRight = "15px";
+};
+
+const removeBodyStyles = () => {
+  document.body.style.overflow = "unset";
+  document.body.style.paddingRight = "0px";
+};
+
 const applicationButton = document.querySelector("#apply-button");
 applicationButton.addEventListener("click", () => {
   applicationModal.style.display = "flex";
-  document.body.style.overflow = "hidden";
-  document.body.style.paddingRight = "15px";
+  addBodyStyles();
 });
 
 const closeApplicationButton = document.querySelector(".application-modal-close-button");
 closeApplicationButton.addEventListener("click", () => {
   applicationModal.style.display = "none";
-  document.body.style.overflow = "unset";
-  document.body.style.paddingRight = "0px";
+  removeBodyStyles();
 });
 
 const searchButton = document.querySelector("#search-button");
@@ -20,8 +28,10 @@ const closeSearchButton = document.querySelector(".search-bar__close-icon");
 
 searchButton.addEventListener("click", () => {
   searchContainer.style.display = "flex";
+  addBodyStyles();
 });
 
 closeSearchButton.addEventListener("click", () => {
   searchContainer.style.display = "none";
+  removeBodyStyles();
 });
