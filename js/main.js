983668de132ec331308228787f9c57baefb8ce79
DvornikovAS <DvornikovAS@admlr.lipetsk.ru>
2026-04-10 10:31:34 +0300

    feat: add body paddings

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@ const applicationButton = document.querySelector("#apply-button");
 applicationButton.addEventListener("click", () => {
   applicationModal.style.display = "flex";
   document.body.style.overflow = "hidden";
+  document.body.style.paddingRight = "15px";
 });
 
 const closeApplicationButton = document.querySelector(".application-modal-close-button");
 closeApplicationButton.addEventListener("click", () => {
   applicationModal.style.display = "none";
-  document.body.style.overflow = "scroll";
+  document.body.style.overflow = "unset";
+  document.body.style.paddingRight = "0px";
 });
