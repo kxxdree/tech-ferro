dafe97f8354f11957a1a69c55ffb29bc65cd1289
kxxdree <truebackward@gmail.com>
2026-04-18 13:00:26 +0300

    feat: wip header component

--- js/header.js
+++ js/header.js
@@ -2,4 +2,18 @@ fetch("/components/header/header.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("header-placeholder").innerHTML = data;
+    
+    const activePage = document.getElementById("header-placeholder").dataset.activePage;
+    
+    document.querySelectorAll('.header__menu-item a').forEach(link => {
+      const href = link.getAttribute('href');
+      const linkPage = href.replace('.html', '').replace('/', '');
+      
+      const menuItem = link.closest('.header__menu-item');
+      menuItem.classList.remove('active-link');
+      
+      if (linkPage === activePage || (activePage === 'index' && href === '/')) {
+        menuItem.classList.add('active-link');
+      }
+    });
   });
