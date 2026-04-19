c5164d8ab1fe0978cb041299dc4f69b524473b2e
kxxdree <truebackward@gmail.com>
2026-04-19 12:18:32 +0300

    feat: add cart success modal

--- js/cart.js
+++ js/cart.js
@@ -37,3 +37,27 @@ function switchToLegal() {
 
 individualBtn.addEventListener("click", switchToIndividual);
 legalBtn.addEventListener("click", switchToLegal);
+
+// Модалка и кнопка оформления заказа
+
+const modal = document.querySelector(".success-modal");
+const orderBtn = document.querySelector(".cart__form-submit-btn");
+
+function openModal() {
+  modal.classList.add("success-modal--open");
+  document.body.style.overflow = "hidden";
+  document.body.style.paddingRight = "15px";
+}
+
+function closeModal() {
+  modal.classList.remove("success-modal--open");
+  document.body.style.overflow = "unset";
+  document.body.style.paddingRight = "0px";
+}
+
+if (orderBtn) {
+  orderBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    openModal();
+  });
+}
