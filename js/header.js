1118bff4bb74757c040c02b5b108a0c66d6560e6
kxxdree <truebackward@gmail.com>
2026-04-20 20:07:08 +0300

    feat: wip search

--- js/header.js
+++ js/header.js
@@ -75,18 +75,21 @@ function initSearch() {
 
   if (searchInput && searchItemsList) {
     const listItemHTML = `
-      <li class="search__success-list-item list__item">
-        <a href="/catalog/id.html" class="list__item-link">
-          <img class="list__item-image" src="/assets/images/search/search-item-photo.png" alt="Фото товара" />
-          <div class="list-item__info">
-            <div class="list-item__info-title">
-              <h4>Костровая чаша</h4>
-              <h3>Fire Cube</h3>
-            </div>
-            <p class="list-item__info-price">3 590 ₽</p>
-          </div>
-        </a>
-      </li>
+     <li class="search__success-list-item list__item">
+  <a href="/catalog/id.html" class="list__item-link">
+    <img class="list__item-image" src="/assets/images/search/search-item-photo.png" alt="Фото товара" />
+    <div class="list-item__info">
+      <div class="list-item__info-title">
+        <h4>Костровая чаша</h4>
+        <h3>Fire Cube</h3>
+      </div>
+      <p class="list-item__info-price">3 590 ₽</p>
+    </div>
+  </a>
+  <button class="list-item__cart-btn" aria-label="Добавить в корзину">
+    <img src="/assets/icons/cart.svg" alt="Корзина" />
+  </button>
+</li>
     `;
 
     for (let i = 0; i < 12; i++) {
