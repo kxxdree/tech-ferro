331985734100e9ed3604b63156466e5ba4d12111
DvornikovAS <DvornikovAS@admlr.lipetsk.ru>
2026-04-21 10:37:41 +0300

    feat: upd styles

--- js/header.js
+++ js/header.js
@@ -130,11 +130,13 @@ function initMenu() {
   burgerButton.addEventListener("click", () => {
     menuOverlay.classList.add("menu--open");
     document.body.style.overflow = "hidden";
+    // document.body.style.paddingRight = "15px";
   });
 
   function closeMenu() {
     menuOverlay.classList.remove("menu--open");
     document.body.style.overflow = "unset";
+    // document.body.style.paddingRight = "0px";
   }
 
   if (closeMenuBtn) {
